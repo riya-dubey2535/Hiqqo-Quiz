c1774b25805783e331c342639baf1fb2076e587a
riya-dubey2535 <riyadubey2635@gmail.com>
2025-04-12 21:20:51 +0530

    Dashboard modified Co-authored-by: Karan Yadav <ky74051@gmail.com>

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -5,37 +5,20 @@ import QuizTypeModal from "./SelectQuizType";
 import { DataProvider } from "../../App";
 import MyLibrary from "./MyLibrary";
 import Reports from "./Reports";
-
-import SearchBar from "./SearchBar";
-import PastQuiz from "./PastQuiz";
-import UpcomingQuiz from "./UpcomingQuiz";
-
 import QuizDashboard from "./QuizDashboard";
-
 
 function Dashboard() {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const { page, setPage } = useContext(DataProvider);
-
+  const {page,setPage} = useContext(DataProvider)
   return (
     <div className="flex h-screen w-full">
       <Sidebar onCreateQuiz={setIsModalOpen} />
       <div className="ml-64 w-full">
         <div className="p-5">
           <QuizTypeModal isOpen={isModalOpen} onClose={setIsModalOpen} />
-
-          <SearchBar />
-          <div className="mt-6">
-            <PastQuiz />
-            <UpcomingQuiz />
-          </div>
-          {page == "my-library" && <MYlibrary />}
-          {page == "reports" && <Reports />}
-
           {page === "dashboard" && <QuizDashboard />}
           {page == 'my-library' && <MyLibrary/>}
           {page == 'reports' && <Reports/>}
-
         </div>
       </div>
     </div>
